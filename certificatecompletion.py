3020a4e20bc6a6fad18bc7d69ff55a35da477c36
Jonathan Harrison <jharrison12@gmail.com>
2016-02-18 11:31:24 -0600

    Added web driver wait to fix not finding element

--- certificatecompletion.py
+++ certificatecompletion.py
@@ -18,7 +18,6 @@ cbrowser = webdriver.Firefox()
 def openArgos():
     #Open Argos web viewer in Firefox and login
     logging.debug("Opening Firefox")
-    cbrowser.implicitly_wait(5)
     cbrowser.get(website)
     loginformUname = WebDriverWait(cbrowser,5).until(EC.element_to_be_clickable((By.ID,'loginUsername')))
     loginformUname.click()
@@ -27,11 +26,15 @@ def openArgos():
     loginformPasswd.click()
     loginformPasswd.send_keys(argosPasswd)
     cbrowser.find_element_by_xpath("//*[@id=\"modalLogin\"]/div[3]/button").click()
-    cbrowser.find_element_by_link_text("Argos Web Viewer").click()
+    argosWebViewer = WebDriverWait(cbrowser,4).until(EC.element_to_be_clickable((By.LINK_TEXT,'Argos Web Viewer')))
+    argosWebViewer.click()
+    #Switch to newest window with the code below
     cbrowser.switch_to.window(cbrowser.window_handles[-1])
-    cbrowser.find_element_by_xpath("//*[@id=\"mCSB_1\"]/div[1]/div[2]/div[2]").click()
-    cbrowser.find_element_by_xpath("//*[@id=\"mCSB_2\"]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]").click()
-    studentID = cbrowser.find_element_by_xpath('//*[@id="datablock"]/div[5]/div[1]/div[25]/div[11]/input')
+    argosStudentView = WebDriverWait(cbrowser,4).until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"mCSB_1\"]/div[1]/div[2]/div[2]")))
+    argosStudentView.click()
+    ultimateStudentview = WebDriverWait(cbrowser, 4).until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"mCSB_2\"]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]")))
+    ultimateStudentview.click()
+    studentID = WebDriverWait(cbrowser,3).until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"datablock\"]/div[5]/div[1]/div[25]/div[11]/input")))
     studentID.click()
     studentID.send_keys("L21743148")
     #TODO iterate over a list of keys and copy the courses to an excel file
@@ -39,21 +42,18 @@ def openArgos():
     Semester.click()
     Search = cbrowser.find_element_by_xpath("//*[@id=\"datablock\"]/div[5]/div[1]/div[25]/div[9]/div/span")
     Search.click()
-    print(cbrowser.find_element_by_xpath("//*[@id=\"datablock\"]/div[5]/div[1]/div[25]/div[12]/div/div[1]/div[2]/div/div[2]/div[1]/div[4]").text)
+    #TODO maybe try except clause here
     for i in range(1,10):
-        if str(cbrowser.find_element_by_xpath("//*[@id=\"datablock\"]/div[5]/div[1]/div[25]/div[12]/div/div[1]/div[2]/div/div[2]/div[" + i + "]/div[4]").text) == "G":
-            cbrowser.find_element_by_xpath("//*[@id=\"datablock\"]/div[5]/div[1]/div[25]/div[12]/div/div[1]/div[2]/div/div[2]/div[" + i + "]/div[4]").click()
+        if cbrowser.find_element_by_xpath("//*[@id=\"datablock\"]/div[5]/div[1]/div[25]/div[12]/div/div[1]/div[2]/div/div[2]/div[" + str(i) + "]/div[4]").text == "G":
+            cbrowser.find_element_by_xpath("//*[@id=\"datablock\"]/div[5]/div[1]/div[25]/div[12]/div/div[1]/div[2]/div/div[2]/div[" + str(i) + "]/div[4]").click()
         else:
             continue
-
-
-#//*[@id="datablock"]/div[5]/div[1]/div[25]/div[12]/div/div[1]/div[2]/div/div[2]/div[1]/div[4]/span
-#xpath selector for whether the student is grad or ug
-#//*[@id="datablock"]/div[5]/div[1]/div[25]/div[12]/div/div[1]/div[2]/div/div[2]/div[2]/div[4]
-
-
-
-
+        break
+    # May not need the lines below because the Classes may already be visibile
+    #Search = cbrowser.find_element_by_xpath("//*[@id=\"datablock\"]/div[5]/div[1]/div[18]/div/span")
+    #Search = WebDriverWait(cbrowser,2).until(EC.element_to_be_clickable((By.XPATH,"//*[@id=\"datablock\"]/div[5]/div[1]/div[18]/div/span")))
+    #Search.click()
+    print(cbrowser.find_element_by_xpath("//*[@id=\"datablock\"]/div[5]/div[6]/div[25]/div[4]/div/div[1]/div[2]/div/div[2]").text)
 
 
 def main():
