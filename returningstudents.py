da7583e224afb855d1a359dae2203959a5c5223a
Jonathan Harrison <jharrison12@gmail.com>
2016-10-04 14:45:17 -0500

    added openxyl module

--- returningstudents.py
+++ returningstudents.py
@@ -2,3 +2,10 @@
 # file that opens an enrollment file and tries to determine
 #how many students are returning
 
+import openpyxl, os, logging
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s- %(message)s')
+
+os.chdir("M:\Working Folder")
+
+wb = openpyxl.load_workbook("Enrollment Fall2014-PResent.xlsx")
+logging.debug("Has opened excel")
