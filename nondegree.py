0ec3465efce0ea3b6d2a7914920d243f455d9c09
Jonathan Harrison <jharrison12@gmail.com>
2017-09-06 14:08:32 -0500

    Program can handle students who didn't take ST

--- nondegree.py
+++ nondegree.py
@@ -3,15 +3,16 @@ File that is supposed to check a students full class academic record.  If they c
 then tagged as a completer for the program.
 """
 
-import openpyxl, os, logging, collections
+import openpyxl, os, logging, collections,pprint
 logging.basicConfig(level=logging.WARNING, format=' %(asctime)s - %(levelname)s- %(message)s')
 
 os.chdir('M:\Rentention\\Nondegree Python Project\\')
 
-wb = openpyxl.load_workbook("Copy.xlsx")
+wb = openpyxl.load_workbook("AllData.xlsx")
 
 sheet = wb.get_active_sheet()
 studentclasses = {}
+yearandprogram = {}
 
 programs = collections.defaultdict(lambda: collections.defaultdict(int))
 
@@ -29,21 +30,37 @@ for row in range(2, sheet.max_row+1):
 	grade = sheet['AS' + str(row)].value
 	program = sheet['AB' + str(row)].value
 	year = sheet['AE' + str(row)].value
-	logging.warning("Row {}: Year: {} Class: {}".format(row,year, classnumber))
+	logging.debug("Row {}: Year: {} Class: {}".format(row,year, classnumber))
 	if(id==nextid):
+		yearandprogram[classnumber] = [program, year]
 		studentclasses[classnumber] = grade
-		logging.debug('Adding {}'.format(classnumber))
+		logging.debug('Adding {}\n'.format(classnumber))
 	else:
+		#The script has found a new student, so the script will check to see if
+		#the student is a completer or not 
 		studentclasses[classnumber] = grade
-		#student teaching
+		#Saves program and year for each class
+		yearandprogram[classnumber] = [program, year]
+		logging.warning("YEAR AND PROGRAM {}\n\n".format(yearandprogram))
+		#Checks to see if student has passed student teaching
 		if(("5417" or "4403" in studentclasses.keys()) and 
 		((studentclasses.get("5417") or studentclasses.get("4403")) != "F")):
-			logging.warning("{}".format(studentclasses))
-			logging.warning("{} found. Adding {}".format(program,year))
-			programs[program][year] += 1
+			logging.debug("{}".format(studentclasses))
+			#Returns the list with the correct program and class number
+			info = [x for x in [yearandprogram.get(key) for key in ["5417","4403"]] if x is not None]
+			logging.warning("{}".format(info))
+			try:
+				newyear = info[0][1]
+				newprogram = info[0][0]
+				logging.warning("{} found. Adding {}".format(newprogram,newyear))
+				programs[newprogram][newyear] += 1
+			#If the student has not taken ST will cause an index out of bound error
+			except:
+				pass
 		studentclasses = {}
+		yearandprogram = {}
 
 
-	
+pprint.pprint(programs)
 
 
