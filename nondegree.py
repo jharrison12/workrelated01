b536e99a1ff833ffbf44238e19fcf5db43cdbd40
Jonathan Harrison <jharrison12@gmail.com>
2017-09-06 12:15:55 -0500

    Data dict set up for number.  File also checks ST for passing grade

--- nondegree.py
+++ nondegree.py
@@ -4,26 +4,46 @@ then tagged as a completer for the program.
 """
 
 import openpyxl, os, logging, collections
+logging.basicConfig(level=logging.WARNING, format=' %(asctime)s - %(levelname)s- %(message)s')
 
 os.chdir('M:\Rentention\\Nondegree Python Project\\')
 
 wb = openpyxl.load_workbook("Copy.xlsx")
 
 sheet = wb.get_active_sheet()
+studentclasses = {}
 
-studentclasses = []
+programs = collections.defaultdict(lambda: collections.defaultdict(int))
 
-for row in range(3, sheet.max_row + 1):
-	id = sheet['B' + str(row)].value
-	previousid = sheet['B'+str(row-1)].value
-	classnumber = sheet['AK'+str(row)].value
-	if(id==previousid):
-		studentclasses.append(classnumber)
-	else:
-		studentclasses = []
+"""
 
-print(studentclasses)
+Maybe a dictionary like {'IP': {"201310":1}}
+
+"""
+
+for row in range(2, sheet.max_row+1):
 	
+	id = sheet['B' + str(row)].value
+	nextid = sheet['B'+str(row+1)].value
+	classnumber = str(sheet['AK'+str(row)].value)
+	grade = sheet['AS' + str(row)].value
+	program = sheet['AB' + str(row)].value
+	year = sheet['AE' + str(row)].value
+	logging.warning("Row {}: Year: {} Class: {}".format(row,year, classnumber))
+	if(id==nextid):
+		studentclasses[classnumber] = grade
+		logging.debug('Adding {}'.format(classnumber))
+	else:
+		studentclasses[classnumber] = grade
+		#student teaching
+		if(("5417" or "4403" in studentclasses.keys()) and 
+		((studentclasses.get("5417") or studentclasses.get("4403")) != "F")):
+			logging.warning("{}".format(studentclasses))
+			logging.warning("{} found. Adding {}".format(program,year))
+			programs[program][year] += 1
+		studentclasses = {}
+
+
 	
 
 
