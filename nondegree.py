3d7da5430ffb12b19ad57c06638da4af9d02f5f2
Jonathan Harrison <jharrison12@gmail.com>
2017-09-07 11:37:10 -0500

    Add logic for ELL Endorsement program

--- nondegree.py
+++ nondegree.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.WARNING, format=' %(asctime)s - %(levelname)s-
 
 os.chdir('M:\Rentention\\Nondegree Python Project\\')
 
-wb = openpyxl.load_workbook("AllData.xlsx")
+wb = openpyxl.load_workbook("Copy.xlsx")
 
 sheet = wb.get_active_sheet()
 studentclasses = {}
@@ -16,14 +16,29 @@ yearandprogram = {}
 
 programs = collections.defaultdict(lambda: collections.defaultdict(int))
 
+ellmed = ["5013","5033","5043","5053"]
+elleds = ["6013","6033","6043","6053"]
+
+"""
+TODO:Maybe check if the subect area if correct. 
+
 """
 
-Maybe a dictionary like {'IP': {"201310":1}}
-
-"""
-
-for row in range(2, sheet.max_row+1):
+#Method that removes failing and incomplete classes from student's dict
+def checkforf(edclasses):
+	logging.debug("EDCLASSES {}".format(edclasses))
+	newclasses = {k:v for k,v in edclasses.items() if (edclasses[k] != "F") or (edclasses[k] != "I")}
+	logging.debug("NEWCLASSES {}".format(newclasses))
+	return newclasses
 	
+def classcompare(edclasses, programclasses):
+	#Frozenset can comare whether one set is a subset 
+	set1 = set([element for element in edclasses.keys()])
+	set2 = set([element for element in programclasses])
+	logging.warning("SET1 {} \n\nSET 2 {} is {}\n\n".format(set1, set2, set2 <= set1))
+	return (set2 <= set1)
+	
+for row in range(2, sheet.max_row+1):
 	id = sheet['B' + str(row)].value
 	nextid = sheet['B'+str(row+1)].value
 	classnumber = str(sheet['AK'+str(row)].value)
@@ -41,26 +56,38 @@ for row in range(2, sheet.max_row+1):
 		studentclasses[classnumber] = grade
 		#Saves program and year for each class
 		yearandprogram[classnumber] = [program, year]
-		logging.warning("YEAR AND PROGRAM {}\n\n".format(yearandprogram))
+		logging.debug("YEAR AND PROGRAM {}\n\n".format(yearandprogram))
+		studentclasses = checkforf(studentclasses)
+		logging.debug(studentclasses)
 		#Checks to see if student has passed student teaching
-		if(("5417" or "4403" in studentclasses.keys()) and 
-		((studentclasses.get("5417") or studentclasses.get("4403")) != "F")):
-			logging.debug("{}".format(studentclasses))
-			#Returns the list with the correct program and class number
+		if (classcompare(studentclasses, ["4403"])
+		 or classcompare(studentclasses, ["5417"])):
+			#Returns the list with the correct program and class number by ignoring None
 			info = [x for x in [yearandprogram.get(key) for key in ["5417","4403"]] if x is not None]
-			logging.warning("{}".format(info))
+			logging.debug("{}".format(info))
 			try:
 				newyear = info[0][1]
 				newprogram = info[0][0]
-				logging.warning("{} found. Adding {}".format(newprogram,newyear))
+				logging.debug("{} found. Adding {}".format(newprogram,newyear))
 				programs[newprogram][newyear] += 1
-			#If the student has not taken ST will cause an index out of bound error
+			#If the student has not taken ST  try block will cause an index out of bound error
 			except:
 				pass
+		elif (classcompare(studentclasses,ellmed)):
+			info = [x for x in [yearandprogram.get(key) for key in ellmed] if x is not None]
+			finalyear = max([element[1] for element in info])
+			logging.warning("ELL info block {} \nand year block {}".format(info,finalyear))
+			try:
+				logging.debug("{} found. Adding {}".format(newprogram,newyear))
+				programs["ELL Endorsement"][finalyear] += 1
+			#If the student has not taken ST  try block will cause an index out of bound error
+			except:
+				pass
+		#TODO DO ell eds
 		studentclasses = {}
 		yearandprogram = {}
+		
 
 
 pprint.pprint(programs)
-
 
